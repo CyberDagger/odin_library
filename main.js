20c5a33ead093ea1143aabb3be92425cd3b35678
CyberDagger <cyberdagger003@gmail.com>
2025-05-29 14:48:52 +0100

    Show list of books in a table

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const listTarget = document.querySelector("#list");
+
 const myLibrary = [];
 
 function Book(title, author) {
@@ -9,3 +11,40 @@ function Book(title, author) {
 function addBookToLibrary(title, author) {
     myLibrary.push(new Book(title, author));
 }
+
+function listBooks(library) {
+    let header = document.createElement("tr");
+    let headElement;
+    headElement = document.createElement("th");
+    headElement.textContent = "ID";
+    header.appendChild(headElement);
+    headElement = document.createElement("th");
+    headElement.textContent = "Title";
+    header.appendChild(headElement);
+    headElement = document.createElement("th");
+    headElement.textContent = "Author";
+    header.appendChild(headElement);
+    listTarget.appendChild(header);
+    for (let i = 0; i < myLibrary.length; i++) {
+        let row = document.createElement("tr");
+        let rowElement;
+        rowElement = document.createElement("td");
+        rowElement.textContent = library[i].id;
+        row.appendChild(rowElement);
+        rowElement = document.createElement("td");
+        rowElement.textContent = library[i].title;
+        row.appendChild(rowElement);
+        rowElement = document.createElement("td");
+        rowElement.textContent = library[i].author;
+        row.appendChild(rowElement);
+        listTarget.appendChild(row);
+    }
+}
+
+addBookToLibrary("The Adventures of Sherlock Holmes", "Arthur Conan Doyle");
+addBookToLibrary("Frankenstein", "Mary Shelley");
+addBookToLibrary("Alice’s Adventures in Wonderland", "Lewis Carroll");
+addBookToLibrary("The Adventures of Sherlock Holmes", "Arthur Conan Doyle");
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien");
+
+listBooks(myLibrary);
