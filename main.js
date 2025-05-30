7c8a116025972b789a6921b4cd5d78bf241c2893
CyberDagger <cyberdagger003@gmail.com>
2025-05-30 14:36:08 +0100

    Record read status

--- main.js
+++ main.js
@@ -12,6 +12,11 @@ function Book(title, author) {
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
+    this.read = false;
+
+    this.toggleRead = function() {
+        this.read = !this.read;
+    }
 }
 
 function addBookToLibrary(library, title, author) {
@@ -34,33 +39,56 @@ function listBooks(library) {
     headElement.textContent = "Author";
     header.appendChild(headElement);
     headElement = document.createElement("th");
+    headElement.textContent = "Read";
+    header.appendChild(headElement);
+    headElement = document.createElement("th");
     headElement.textContent = "Delete";
     header.appendChild(headElement);
     listTarget.appendChild(header);
     for (let i = 0; i < myLibrary.length; i++) {
         let row = document.createElement("tr");
         let rowElement;
+        // ID
         rowElement = document.createElement("td");
         rowElement.textContent = library[i].id;
         row.appendChild(rowElement);
+        // Title
         rowElement = document.createElement("td");
         rowElement.textContent = library[i].title;
         row.appendChild(rowElement);
+        // Author
         rowElement = document.createElement("td");
         rowElement.textContent = library[i].author;
         row.appendChild(rowElement);
+        // Read
+        rowElement = document.createElement("td");
+        let readChk = document.createElement("input");
+        readChk.setAttribute("type", "checkbox");
+        readChk.setAttribute("data-id", library[i].id);
+        if (library[i].read) {
+            readChk.setAttribute("checked", "checked");
+        }
+        readChk.addEventListener("click", () => {
+            const index = library.map(i => i.id).indexOf(library[i].id);
+            library[i].toggleRead();
+            listBooks(library);
+        })
+        rowElement.appendChild(readChk);
+        row.appendChild(rowElement);
+        // Delete
         rowElement = document.createElement("td");
         let delBtn = document.createElement("button");
         delBtn.textContent = "X";
         delBtn.classList.add("delete");
         delBtn.setAttribute("data-id", library[i].id);
-        delBtn.addEventListener("click", (event) => {
+        delBtn.addEventListener("click", () => {
             const index = library.map(i => i.id).indexOf(library[i].id);
             library.splice(index, 1);
             listBooks(library);
         })
         rowElement.appendChild(delBtn);
         row.appendChild(rowElement);
+
         listTarget.appendChild(row);
     }
 }
