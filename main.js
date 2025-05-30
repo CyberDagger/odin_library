9f7c03f42f89cf55927459306a5db45cec5a2403
CyberDagger <cyberdagger003@gmail.com>
2025-05-30 13:06:22 +0100

    Allow user to add books

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const listTarget = document.querySelector("#list");
 const buttonNewBook = document.querySelector("#btn-new");
-const buttonSubmit = document.querySelector("btn-submit-book");
+const buttonSubmit = document.querySelector("#btn-submit-book");
 const buttonClose = document.querySelector("#btn-close-form");
 const submitForm = document.querySelector("#submit-form");
+const inputTitle = document.querySelector("#title");
+const inputAuthor = document.querySelector("#author");
 
 const myLibrary = [];
 
@@ -12,8 +14,8 @@ function Book(title, author) {
     this.author = author;
 }
 
-function addBookToLibrary(title, author) {
-    myLibrary.push(new Book(title, author));
+function addBookToLibrary(library, title, author) {
+    library.push(new Book(title, author));
 }
 
 function listBooks(library) {
@@ -28,6 +30,9 @@ function listBooks(library) {
     headElement = document.createElement("th");
     headElement.textContent = "Author";
     header.appendChild(headElement);
+    headElement = document.createElement("th");
+    headElement.textContent = "Delete";
+    header.appendChild(headElement);
     listTarget.appendChild(header);
     for (let i = 0; i < myLibrary.length; i++) {
         let row = document.createElement("tr");
@@ -41,6 +46,11 @@ function listBooks(library) {
         rowElement = document.createElement("td");
         rowElement.textContent = library[i].author;
         row.appendChild(rowElement);
+        rowElement = document.createElement("td");
+        let delBtn = document.createElement("button");
+        delBtn.textContent = "X";
+        rowElement.appendChild(delBtn);
+        row.appendChild(rowElement);
         listTarget.appendChild(row);
     }
 }
@@ -53,10 +63,22 @@ buttonClose.addEventListener("click", () => {
     submitForm.close();
 })
 
-addBookToLibrary("The Adventures of Sherlock Holmes", "Arthur Conan Doyle");
-addBookToLibrary("Frankenstein", "Mary Shelley");
-addBookToLibrary("Alice’s Adventures in Wonderland", "Lewis Carroll");
-addBookToLibrary("The Adventures of Sherlock Holmes", "Arthur Conan Doyle");
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien");
+buttonSubmit.addEventListener("click", (event) => {
+    event.preventDefault();
+    submitForm.close();
+    addBookToLibrary(myLibrary, inputTitle.value, inputAuthor.value);
+    while (listTarget.firstChild) {
+        listTarget.removeChild(listTarget.lastChild);
+    }
+    listBooks(myLibrary);
+    inputTitle.value = "";
+    inputAuthor.value = "";
+})
+
+addBookToLibrary(myLibrary, "The Adventures of Sherlock Holmes", "Arthur Conan Doyle");
+addBookToLibrary(myLibrary, "Frankenstein", "Mary Shelley");
+addBookToLibrary(myLibrary, "Alice’s Adventures in Wonderland", "Lewis Carroll");
+addBookToLibrary(myLibrary, "The Adventures of Sherlock Holmes", "Arthur Conan Doyle");
+addBookToLibrary(myLibrary, "The Hobbit", "J.R.R. Tolkien");
 
 listBooks(myLibrary);
